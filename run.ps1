f9e4f48512505d8e9c66d54becfb0dabaac14364
silentaimv1-png <silentaimv1@gmail.com>
2026-05-15 22:07:05 +0700

    run.ps1

--- run.ps1
+++ run.ps1
@@ -2,7 +2,7 @@ Write-Host "BASX" -ForegroundColor White
 
 $url = "https://github.com/silentaimv1-png/basxpanel/raw/refs/heads/main/BASX.exe"
 
-$output = "$env:TEMP\BASX_Panel.exe"
+$output = "$env:TEMP\BASX.exe"
 
 Write-Host "[*] Downloading BASX System..." -ForegroundColor Yellow
 
